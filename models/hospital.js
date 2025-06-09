5437d5ec26ca6c05b8a59d25d98c7eb5a5fe0d49
Anukrishna2004 <anukrishna1962@gmail.com>
2025-06-09 16:05:47 +0530

    hosiptal commited

---
+++ models/hospital.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const hospitalschema = new mongoose.hospitalschema({
+    name:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    gender:{
+        type:String,
+        enum: ['Male', 'Female', 'other'],
+        required:true
+    },
+    phoneNumber:{
+        type:String,
+        required:true,
+        match: /^[0-9]{10}$/
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        match:/\S+@\S+\.\S+/
+    },
+    bloodGroup:{
+        type:String,
+        enum:['A+','A-','B+','B-','AB+','AB-','O+','O-'],
+        required:true
+    },
+    guardianName:{
+        type:String,
+        required:true,
+    },
+    guardianNumber:{
+        type:String,
+        required:true,
+        match:/^[0-9]{10}$/
+    }
+});
+module.exports = mongoose.model('HospitalUser',hospitalschema);
