747fcfefdca6f6546faa3356b590c7aea0b9c46a
ami2004-asok <amiasokaithara@gmail.com>
2025-06-09 12:34:10 +0530

    signin commit cheytu

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose');
+const UserSchema= new mongoose.Schema({
+    Username:{ type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    role: {
+    type: String,
+    enum: ['User', 'Admin', 'SuperAdmin'],
+    default: 'visitor'
+  },
+
+})
