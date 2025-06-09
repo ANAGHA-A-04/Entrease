76bf8c7e12e56535e36bf846efe2a1c7f26739b3
ANAGHA-A-04 <anagha8848@gmail.com>
2025-06-09 16:22:30 +0530

    school commited

---
+++ models/school.js
@@ -0,0 +1,32 @@
+const mongoose=require("mongoose");
+const schoolschema= new mongoose.Schema({
+    name:{type:String,required:true},
+    DOB:{type:Date,required:true},
+    gender:{
+        type:String,
+        enum:['male','female','other'],
+        required:true
+    },
+    class:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    guardian:{
+        type:String,
+        required:true
+    },
+    relation:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        match:[/^[0-9]{10}$/,'phone number must be 10 digits']
+    },
+}, { timestamps: true });
+module.exports=mongoose.model("student",schoolschema);
